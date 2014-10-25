48e9eefeed8924b6ea79817f3f4980057c15bc83
Loushou <chris@quadshot.com>
2014-10-24 17:46:00 -0700

    removed cruft from general settings page added styling to the settings pages moved settings to appropriate new settings pages added settings for frontend colors added ticket option for order number on ticket added ticket option for image to use on ticket added ending date to ticket removed frontend event.css file, and made it create on activate & settings save loushou

--- inc/order/admin.class.php
+++ inc/order/admin.class.php
@@ -588,8 +588,8 @@ class qsot_order_admin {
 		self::$options->add(array(
 			'order' => 2100,
 			'type' => 'title',
-			'title' => __('Additional Admin Based Order Validation', 'qsot'),
-			'id' => 'heading-admin-orders-1',
+			'title' => __('Additional Email Settings', 'qsot'),
+			'id' => 'heading-add-email-sets-1',
 		));
 
 		self::$options->add(array(
@@ -615,7 +615,7 @@ class qsot_order_admin {
 		self::$options->add(array(
 			'order' => 2199,
 			'type' => 'sectionend',
-			'id' => 'heading-admin-orders-1',
+			'id' => 'heading-add-email-sets-1',
 		));
 	}
 }
