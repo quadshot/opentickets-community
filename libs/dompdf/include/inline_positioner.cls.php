703054bab7c0fa3ff9300bffb3bd4580de65d142
Loushou <chris@quadshot.com>
2015-08-26 16:19:12 -0700

    adding a catch to control flow a little better on debug loushou

--- libs/dompdf/include/inline_positioner.cls.php
+++ libs/dompdf/include/inline_positioner.cls.php
@@ -36,9 +36,11 @@ class Inline_Positioner extends Positioner {
     if ( !$p ) {
 			if ( function_exists( 'current_user_can' ) && current_user_can( 'edit_posts' ) ) {
 				echo '<pre>';
-				var_dump( get_class( $this->_frame), debug_backtrace() );
-				ini_set( 'memory_limit', '256M' );
-				var_dump($this->_frame);
+				var_dump( get_class( $this->_frame ), debug_backtrace() );
+				if ( isset( $_GET['more'] ) && 'yes' == $_GET['more'] ) {
+					ini_set( 'memory_limit', '256M' );
+					var_dump($this->_frame);
+				}
 				echo '</pre>';
 			}
       throw new DOMPDF_Exception("No block-level parent found.  Not good.");
