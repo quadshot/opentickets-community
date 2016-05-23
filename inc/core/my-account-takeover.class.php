f4fc32c20fba0612b9e7c85c38d46f24a071c850
Loushou <chris@quadshot.com>
2016-05-23 07:47:29 -0700

    fixing timestamp issue on upcoming tickets display loushou

--- inc/core/my-account-takeover.class.php
+++ inc/core/my-account-takeover.class.php
@@ -217,51 +217,68 @@ class qsot_my_account_takeover {
 		}
 	}
 
+	// renders the list of upcoming event tickets for the current user, based on their recent orders
 	public static function draw_upcoming_event_tickets_list($current_user) {
 		global $wpdb;
 
-		$orders = get_posts(array(
+		// get a list of all the order ids for the current user
+		$orders = get_posts( array(
 			'posts_per_page' => -1,
 			'meta_key' => '_customer_user',
-			'meta_value' => is_object($current_user) && isset($current_user->ID) ? $current_user->ID : get_current_user_id(),
+			'meta_value' => is_object( $current_user ) && isset( $current_user->ID ) ? $current_user->ID : get_current_user_id(),
 			'post_type' => 'shop_order',
 			'post_status' => 'any',
 			'fields' => 'ids',
-		));
-		if (!is_array($orders) || empty($orders)) return;
-		$orders = array_map('absint', $orders);
+		) );
+		$orders = is_array( $orders ) ? array_filter( $orders ) : array();
 
-		$q = 'select distinct order_item_id from '.$wpdb->base_prefix.'woocommerce_order_items where order_id in ('.implode(',', $orders).')';
-		$order_item_ids = $wpdb->get_col($q);
-		if (!is_array($order_item_ids) || empty($order_item_ids)) return;
-		$order_item_ids = array_map('absint', $order_item_ids);
+		// if there are no orders, then bail and draw nothing
+		if ( empty( $orders ) )
+			return;
+		$orders = array_map( 'absint', $orders );
 
+		// find all the order items for the given orders
+		$q = 'select distinct order_item_id from ' . $wpdb->base_prefix . 'woocommerce_order_items where order_id in (' . implode( ',', $orders ) . ')';
+		$order_item_ids = $wpdb->get_col( $q );
+
+		// if there are no order items, then bail
+		if ( ! is_array( $order_item_ids ) || empty( $order_item_ids ) )
+			return;
+		$order_item_ids = array_map( 'absint', $order_item_ids );
+
+		// grab all the meta for the found order items, where the meta points to an event_id
 		$q = $wpdb->prepare(
-			'select order_item_id, meta_value from '.$wpdb->base_prefix.'woocommerce_order_itemmeta where order_item_id in ('.implode(',', $order_item_ids).') and meta_key = %s',
+			'select order_item_id, meta_value from ' . $wpdb->base_prefix . 'woocommerce_order_itemmeta where order_item_id in (' . implode( ',', $order_item_ids ) . ') and meta_key = %s',
 			'_event_id'
 		);
-		$pairs = $wpdb->get_results($q);
-		if (!is_array($pairs) || empty($pairs)) return;
+		$pairs = $wpdb->get_results( $q );
+
+		// if there is no meta found, then bail
+		if ( ! is_array( $pairs ) || empty( $pairs ) )
+			return;
 
 		$groups = array();
+		// aggregate a list of the event ids that this user has an order item for
 		foreach ($pairs as $pair) {
 			$event_id = $pair->meta_value;
 			$oiid = $pair->order_item_id;
-			if (!isset($groups["{$event_id}"]) || !is_array($groups["{$event_id}"])) $groups["{$event_id}"] = array();
+			if ( ! isset( $groups["{$event_id}"] ) || ! is_array( $groups["{$event_id}"] ) )
+				$groups["{$event_id}"] = array();
 			$groups["{$event_id}"][] = $oiid;
 		}
 
+		// find all the events that match this user's upcoming tickets list
 		$events = get_posts(array(
 			'posts_per_page' => -1,
 			'fields' => 'ids',
 			'suppress_filters' => false,
-			'post_status' => current_user_can( 'read_private_posts' ) ? array('publish', 'hidden', 'private') : array( 'publish' ),
+			'post_status' => current_user_can( 'read_private_posts' ) ? array( 'publish', 'hidden', 'private' ) : array( 'publish' ),
 			'post_type' => self::$o->core_post_type,
-			'post__in' => array_keys($groups),
+			'post__in' => array_keys( $groups ),
 			'meta_query' => array(
 				array(
 					'key' => self::$o->{'meta_key.start'},
-					'value' => date('Y-m-d H:i:s'),
+					'value' => date( 'Y-m-d H:i:s', QSOT_Utils::local_timestamp( date( 'c' ) ) ),
 					'type' => 'DATETIME',
 					'compare' => '>=',
 				),
@@ -270,58 +287,81 @@ class qsot_my_account_takeover {
 			'orderby' => 'meta_value_date',
 			'order' => 'asc',
 		));
-		if (!is_array($events) || empty($events)) return;
-		$events = array_map('absint', $events);
+
+		// if we found no events, then bail
+		if ( ! is_array( $events ) || empty( $events ) )
+			return;
+		$events = array_map( 'absint', $events );
 
 		$ticket_ids = array();
-		foreach ($events as $eid)
-			if (isset($groups["{$eid}"]))
-				$ticket_ids = array_merge($ticket_ids, $groups["{$eid}"]);
-		$ticket_ids = array_unique($ticket_ids);
+		// get a unique list of order items that match the found events
+		foreach ( $events as $eid )
+			if ( isset( $groups["{$eid}"] ) )
+				$ticket_ids = array_merge( $ticket_ids, $groups["{$eid}"] );
+		$ticket_ids = array_unique( $ticket_ids );
 
-		$q = 'select * from '.$wpdb->base_prefix.'woocommerce_order_itemmeta where order_item_id in ('.implode(',', $ticket_ids).')';
+		// get all the meta for the found items
+		$q = 'select * from ' . $wpdb->base_prefix . 'woocommerce_order_itemmeta where order_item_id in (' . implode( ',', $ticket_ids ) . ')';
 		$raw_data = $wpdb->get_results($q);
 
-		$q = 'select order_id, order_item_id from '.$wpdb->base_prefix.'woocommerce_order_items where order_item_id in ('.implode(',', $ticket_ids).')';
-		$raw_pairs = $wpdb->get_results($q);
+		// figure out what orders each item links to
+		$q = 'select order_id, order_item_id from ' . $wpdb->base_prefix . 'woocommerce_order_items where order_item_id in (' . implode( ',', $ticket_ids ) . ')';
+		$raw_pairs = $wpdb->get_results( $q );
 		$pairs = array();
-		foreach ($raw_pairs as $raw_row) $pairs[$raw_row->order_item_id.''] = $raw_row->order_id;
+		foreach ( $raw_pairs as $raw_row )
+			$pairs[ $raw_row->order_item_id . '' ] = $raw_row->order_id;
 
 		$e_data = $event_data = $ticket_data = array();
+		// index all the meta by order_item_id, and make sure to assing the order_id and order_item_id values as hidden meta for backreference later
 		foreach ($raw_data as $row) {
 			if (!isset($ticket_data["{$row->order_item_id}"]) || !is_array($ticket_data["{$row->order_item_id}"]))
 				$ticket_data["{$row->order_item_id}"] = array('__order_item_id' => $row->order_item_id, '__order_id' => isset($pairs[$row->order_item_id]) ? $pairs[$row->order_item_id] : 0);
 			$ticket_data["{$row->order_item_id}"][$row->meta_key] = $row->meta_value;
 		}
 
-		foreach ($ticket_data as $ind => $ticket) {
-			$ticket = (object)wp_parse_args($ticket, array(
+		// normalize all ticket information, and add all extra data about each ticket we need for display
+		foreach ( $ticket_data as $ind => $ticket ) {
+			// normalize the ticket data
+			$ticket = (object) wp_parse_args( $ticket, array(
 				'_ticket_code' => '',
 				'_ticket_link' => '',
 				'_product_id' => 0,
 				'_event_id' => 0,
 				'__order_id' => 0,
-			));
-			$ticket->permalink = apply_filters('qsot-get-ticket-link', '', $ticket->__order_item_id);
-			$ticket->product = get_product($ticket->_product_id);
-			$ticket->event = apply_filters('qsot-event-add-meta', get_post($ticket->_event_id));
-			$ticket_data[$ind] = $ticket;
+			) );
 
-			if (is_object($ticket->event) && (!isset($e_data["{$ticket->_event_id}"]) || !is_object($e_data["{$ticket->_event_id}"])))
+			// add the permalink that points to the ticket itself
+			$ticket->permalink = apply_filters( 'qsot-get-ticket-link', '', $ticket->__order_item_id );
+
+			// load the ticket product information
+			$ticket->product = wc_get_product( $ticket->_product_id );
+
+			// load the full event
+			$ticket->event = apply_filters( 'qsot-event-add-meta', get_post( $ticket->_event_id ) );
+
+			// store all this date in the results array
+			$ticket_data[ $ind ] = $ticket;
+
+			// aggregate master lists of all the events and all the tickets, for use in the template
+			if ( is_object( $ticket->event ) && ( ! isset( $e_data["{$ticket->_event_id}"] ) || ! is_object( $e_data["{$ticket->_event_id}"] ) ) )
 				$e_data["{$ticket->_event_id}"] = $ticket->event;
-			if (!isset($e_data["{$ticket->_event_id}"]->tickets) || !is_array($e_data["{$ticket->_event_id}"]->tickets))
+			if ( ! isset( $e_data["{$ticket->_event_id}"]->tickets ) || ! is_array( $e_data["{$ticket->_event_id}"]->tickets ) )
 				$e_data["{$ticket->_event_id}"]->tickets = array();
 			$e_data["{$ticket->_event_id}"]->tickets[] = $ticket;
 		}
 
-		foreach ($events as $eid) if (isset($e_data[$eid.''])) $event_data[$eid.''] = $e_data[$eid.''];
+		// normalize the master list of events that this user has tickets upcoming for
+		foreach ( $events as $eid )
+			if ( isset( $e_data[ $eid . '' ] ) )
+				$event_data[ $eid . '' ] = $e_data[ $eid . '' ];
 
-		wc_get_template('myaccount/my-upcoming-tickets.php', array(
+		// render the template
+		wc_get_template( 'myaccount/my-upcoming-tickets.php', array(
 			'user' => $current_user,
 			'tickets' => $ticket_data,
 			'by_event' => $event_data,
 			'display_format' => self::$options->{'qsot-my-account-display-upcoming-tickets'},
-		));
+		) );
 	}
 
 	protected static function _setup_admin_options() {
