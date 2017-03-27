8610a4f1448e809c70cdd12769d7e95a612f858f
Loushou <chris@quadshot.com>
2017-03-27 08:01:23 -0700

    updating changelog and version to 2.7.0.2 loushou

--- launcher.php
+++ launcher.php
@@ -3,7 +3,7 @@
  * Plugin Name: OpenTickets Community Edition
  * Plugin URI:  http://opentickets.com/
  * Description: Event Management and Online Ticket Sales Platform
- * Version:     2.7.0.1
+ * Version:     2.7.0.2
  * Author:      Quadshot Software LLC
  * Author URI:  http://quadshot.com/
  * Copyright:   Copyright (C) 2009-2014 Quadshot Software LLC
@@ -53,7 +53,7 @@ class opentickets_community_launcher {
 			'pre' => 'qsot-',
 			'fctm' => 'fc',
 			'always_reserve' => 0,
-			'version' => '2.7.0.1',
+			'version' => '2.7.0.2',
 			'min_wc_version' => '2.6.14',
 			'core_post_type' => 'qsot-event',
 			'core_post_rewrite_slug' => 'event',
--- readme.txt
+++ readme.txt
@@ -171,9 +171,10 @@ The FAQ's for OpenTickets Community Edition is currently located on [our website
 
 == Changelog ==
 
-= 2.7.0.1 - Mar/24/2017 =
+= 2.7.0.1 - Mar/27/2017 =
 * [tweak] changing how dates are calculated for DST
 * [tweak] adjusted event save functions to calculate the proper UTC version of the start time, under certain circumstances
+* [fix] repaired 'event sales stop time' calculation
 
 = 2.7.0 - Mar/14/2017 =
 * [new] added ability to use qtranslate-x on date format fields in settings
